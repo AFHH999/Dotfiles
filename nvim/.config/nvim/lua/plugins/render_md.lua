b38dfe20c180285284c14df08ce35d464607a31a
Felipe <ah193971@gmail.com>
2026-02-05 15:04:10 -0600

    Adding a markdown reader

--- nvim/.config/nvim/lua/plugins/render_md.lua
+++ nvim/.config/nvim/lua/plugins/render_md.lua
@@ -1,9 +1,51 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
+
+  -- Load only when it makes sense
+  ft = { "markdown" },
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-mini/mini.nvim",
+  },
+
+  ---@module "render-markdown"
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    heading = {
+      sign = false, -- cleaner gutter
+      icons = {
+        "󰲡 ",
+        "󰲣 ",
+        "󰲥 ",
+        "󰲧 ",
+        "󰲩 ",
+        "󰲫 ",
+      },
+    },
+
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 4,
+    },
+  },
+
+  config = function(_, opts)
+    local ok, render_md = pcall(require, "render-markdown")
+    if not ok then
+      return
+    end
+
+    render_md.setup(opts)
+
+    -- Localize conceal settings to markdown buffers only
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "markdown",
+      callback = function()
+        vim.opt_local.conceallevel = 2
+        vim.opt_local.concealcursor = "nc"
+      end,
+    })
+  end,
 }
